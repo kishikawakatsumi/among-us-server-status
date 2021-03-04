d86d03910db0458b281074cd39f05ceddf233d50
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2021-03-05 03:01:26 +0900

    Refactor and styling

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,7 +1,7 @@
 import Vapor
 import AmongUsProtocol
 
-private var lastUpdateTimestamp: TimeInterval = 0
+private var lastUpdate: TimeInterval = 0
 private var cachedResponse: IndexResponse?
 
 func routes(_ app: Application) throws {
@@ -10,122 +10,83 @@ func routes(_ app: Application) throws {
     }
 
     func index(_ req: Request) throws -> EventLoopFuture<View> {
-        if var cachedResponse = cachedResponse, Date().timeIntervalSince1970 - lastUpdateTimestamp < 60 {
-            let dateFormatter = ISO8601DateFormatter()
-            let lastUpdate = dateFormatter.string(from: Date(timeIntervalSince1970: lastUpdateTimestamp))
+        if var cachedResponse = cachedResponse, Date().timeIntervalSince1970 - lastUpdate < 60 {
             cachedResponse.lastUpdate = lastUpdate
-
             return req.view.render("index", cachedResponse)
         }
 
         let promise = req.eventLoop.makePromise(of: View.self)
 
-        let evGroup = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
-        var futures = [EventLoopFuture<Data>]()
-
-        let numberFormatter = NumberFormatter()
-        numberFormatter.numberStyle = .decimal
-
         var clients = [UDPClient]()
+        let futures = try fetchStatuses(clients: &clients)
 
-        for (address, port) in [("104.237.135.186", 22023), ("139.162.111.196", 22023), ("172.105.251.170", 22023)] {
-            let client = try UDPClient(address, port: port, bindHost: "0.0.0.0", bindPort: 0)
-            clients.append(client)
-            let future = client.execute(Data(hex: "080001004ae202030a496e6e657273726f7468")!)
-            futures.append(future)
-        }
-
-        let futureResult = EventLoopFuture.reduce([], futures, on: evGroup.next()) { (servers, data) -> [[String: String]] in
-            let packet = PacketParser.parse(packet: data)
-            if case .normal(let packet) = packet {
-                if case .reselectServer(let reselectServer)  = packet.messages[0].payload {
-                    let masterServers = reselectServer
-                        .masterServers
-                        .sorted { $0.name < $1.name }
-                        .map { (masterServer) -> [String: String] in
-                            ["region": masterServer.name.starts(with: "Asia") ? "Asia" : masterServer.name.starts(with: "Europe") ? "Europe" : "North America",
-                             "name": masterServer.name,
-                             "ipAddress": "\(masterServer.ipAddress)",
-                             "port": "\(masterServer.port)",
-                             "numberOfConnections": "\(numberFormatter.string(from: NSNumber(value: masterServer.numberOfConnections))!)",
-                             "order": masterServer.name.starts(with: "Asia") ? "2" : masterServer.name.starts(with: "Europe") ? "3" : "1",
-                            ]
-                    }
-                    return servers + masterServers
-                }
-            }
-            return [[:]]
-        }
-
-        futureResult.whenComplete {
-            defer { clients.removeAll() }
-
-            switch $0 {
-            case .success(let servers):
+        let group = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
+        EventLoopFuture
+            .reduce([], futures, on: group.next()) { $0 + parsePacket($1) }
+            .whenComplete {
                 defer { clients.removeAll() }
-                lastUpdateTimestamp = Date().timeIntervalSince1970
 
-                let dateFormatter = ISO8601DateFormatter()
-                let lastUpdate = dateFormatter.string(from: Date(timeIntervalSince1970: lastUpdateTimestamp))
+                switch $0 {
+                case .success(let servers):
+                    defer { clients.removeAll() }
+                    lastUpdate = Date().timeIntervalSince1970
 
-                let servers = Dictionary(grouping: servers) { $0["region"]! }
-                    .values
-                    .map { $0 }
-                    .sorted { $0[0]["order"]! < $1[0]["order"]! }
+                    let response = buildIndexResponse(servers: servers)
+                    cachedResponse = response
 
-                let response = IndexResponse(servers: servers, lastUpdate: lastUpdate)
-                cachedResponse = response
-
-                return req.view.render("index", response)
+                    return req.view.render(
+                        "index", response
+                    )
                     .cascade(to: promise)
-            case .failure(let error):
-                promise.fail(error)
-            }
+                case .failure(let error):
+                    promise.fail(error)
+                }
         }
 
         return promise.futureResult
     }
 
     app.get("discord-embed") { (req) -> EventLoopFuture<DiscordEmbedResponse> in
-        if var cachedResponse = cachedResponse, Date().timeIntervalSince1970 - lastUpdateTimestamp < 60 {
-            let dateFormatter = ISO8601DateFormatter()
-            let lastUpdate = dateFormatter.string(from: Date(timeIntervalSince1970: lastUpdateTimestamp))
+        if var cachedResponse = cachedResponse, Date().timeIntervalSince1970 - lastUpdate < 60 {
             cachedResponse.lastUpdate = lastUpdate
-
-            var fields = [DiscordEmbedField]()
-            for server in cachedResponse.servers {
-                fields.append(
-                    DiscordEmbedField(
-                        name: "Region",
-                        value: "\(server[0]["region"]!)",
-                        inline: false)
-                )
-                for s in server {
-                    fields.append(
-                        DiscordEmbedField(
-                            name: "\(s["ipAddress"]!)",
-                            value: "\(s["numberOfConnections"]!) players\n ",
-                            inline: true)
-                    )
-                }
-            }
-
             return req.eventLoop.makeSucceededFuture(
-                DiscordEmbedResponse(
-                    embed: DiscordEmbed(timestamp: "\(dateFormatter.string(from: Date(timeIntervalSince1970: lastUpdateTimestamp)))", fields: fields)
-                )
+                buildDiscordEmbed(response: cachedResponse)
             )
         }
 
         let promise = req.eventLoop.makePromise(of: DiscordEmbedResponse.self)
 
-        let evGroup = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
-        var futures = [EventLoopFuture<Data>]()
-
-        let numberFormatter = NumberFormatter()
-        numberFormatter.numberStyle = .decimal
-
         var clients = [UDPClient]()
+        let futures = try fetchStatuses(clients: &clients)
+
+        let group = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
+        EventLoopFuture
+            .reduce([], futures, on: group.next()) { $0 + parsePacket($1) }
+            .whenComplete {
+                defer { clients.removeAll() }
+
+                switch $0 {
+                case .success(let servers):
+                    defer { clients.removeAll() }
+                    lastUpdate = Date().timeIntervalSince1970
+
+                    let response = buildIndexResponse(servers: servers)
+                    cachedResponse = response
+
+                    return req.eventLoop.makeSucceededFuture(
+                        buildDiscordEmbed(response: response)
+                    )
+                    .cascade(to: promise)
+                case .failure(let error):
+                    promise.fail(error)
+                }
+        }
+
+        return promise.futureResult
+    }
+
+    func fetchStatuses(clients: inout [UDPClient]) throws -> [EventLoopFuture<Data>] {
+        var futures = [EventLoopFuture<Data>]()
 
         for (address, port) in [("104.237.135.186", 22023), ("139.162.111.196", 22023), ("172.105.251.170", 22023)] {
             let client = try UDPClient(address, port: port, bindHost: "0.0.0.0", bindPort: 0)
@@ -134,83 +95,77 @@ func routes(_ app: Application) throws {
             futures.append(future)
         }
 
-        let futureResult = EventLoopFuture.reduce([], futures, on: evGroup.next()) { (servers, data) -> [[String: String]] in
-            let packet = PacketParser.parse(packet: data)
-            if case .normal(let packet) = packet {
-                if case .reselectServer(let reselectServer)  = packet.messages[0].payload {
-                    let masterServers = reselectServer
-                        .masterServers
-                        .sorted { $0.name < $1.name }
-                        .map { (masterServer) -> [String: String] in
-                            ["region": masterServer.name.starts(with: "Asia") ? "Asia" : masterServer.name.starts(with: "Europe") ? "Europe" : "North America",
-                             "name": masterServer.name,
-                             "ipAddress": "\(masterServer.ipAddress)",
-                             "port": "\(masterServer.port)",
-                             "numberOfConnections": "\(numberFormatter.string(from: NSNumber(value: masterServer.numberOfConnections))!)",
-                             "order": masterServer.name.starts(with: "Asia") ? "2" : masterServer.name.starts(with: "Europe") ? "3" : "1",
-                            ]
-                    }
-                    return servers + masterServers
+        return futures
+    }
+
+    func parsePacket(_ data: Data) -> [[String: String]] {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+
+        let packet = PacketParser.parse(packet: data)
+        if case .normal(let packet) = packet {
+            if case .reselectServer(let reselectServer)  = packet.messages[0].payload {
+                let masterServers = reselectServer
+                    .masterServers
+                    .sorted { $0.name < $1.name }
+                    .map { (masterServer) -> [String: String] in
+                        ["region": masterServer.name.starts(with: "Asia") ? "Asia" : masterServer.name.starts(with: "Europe") ? "Europe" : "North America",
+                         "regionEmoji": masterServer.name.starts(with: "Asia") ? "🌏" : masterServer.name.starts(with: "Europe") ? "🌍" : "🌎",
+                         "name": masterServer.name,
+                         "ipAddress": "\(masterServer.ipAddress)",
+                         "port": "\(masterServer.port)",
+                         "numberOfConnections": "\(numberFormatter.string(from: NSNumber(value: masterServer.numberOfConnections))!)",
+                         "order": masterServer.name.starts(with: "Asia") ? "2" : masterServer.name.starts(with: "Europe") ? "3" : "1",
+                        ]
                 }
-            }
-            return [[:]]
-        }
-
-        futureResult.whenComplete {
-            defer { clients.removeAll() }
-
-            switch $0 {
-            case .success(let servers):
-                defer { clients.removeAll() }
-                lastUpdateTimestamp = Date().timeIntervalSince1970
-
-                let dateFormatter = ISO8601DateFormatter()
-                let lastUpdate = dateFormatter.string(from: Date(timeIntervalSince1970: lastUpdateTimestamp))
-
-                let servers = Dictionary(grouping: servers) { $0["region"]! }
-                    .values
-                    .map { $0 }
-                    .sorted { $0[0]["order"]! < $1[0]["order"]! }
-
-                let response = IndexResponse(servers: servers, lastUpdate: lastUpdate)
-                cachedResponse = response
-
-                var fields = [DiscordEmbedField]()
-                for server in response.servers {
-                    fields.append(
-                        DiscordEmbedField(
-                            name: "Region",
-                            value: "\(server[0]["region"]!)",
-                            inline: false)
-                    )
-                    for s in server {
-                        fields.append(
-                            DiscordEmbedField(
-                                name: "\(s["ipAddress"]!)",
-                                value: "\(s["numberOfConnections"]!) players\n ",
-                                inline: true)
-                        )
-                    }
-                }
-
-                return req.eventLoop.makeSucceededFuture(
-                    DiscordEmbedResponse(
-                        embed: DiscordEmbed(timestamp: "\(dateFormatter.string(from: Date(timeIntervalSince1970: lastUpdateTimestamp)))", fields: fields)
-                    )
-                )
-                .cascade(to: promise)
-            case .failure(let error):
-                promise.fail(error)
+                return masterServers
             }
         }
+        return [[:]]
+    }
 
-        return promise.futureResult
+    func buildIndexResponse(servers: [[String : String]]) -> IndexResponse {
+        let servers = Dictionary(grouping: servers) { $0["region"]! }
+            .values
+            .map { $0 }
+            .sorted { $0[0]["order"]! < $1[0]["order"]! }
+
+        return IndexResponse(servers: servers, lastUpdate: lastUpdate)
+    }
+
+    func buildDiscordEmbed(response: IndexResponse) -> DiscordEmbedResponse {
+        var fields = [DiscordEmbedField]()
+        for server in response.servers {
+            let value = """
+            ```
+            \(
+                server
+                    .map { "\($0["ipAddress"]!.padding(toLength: 15, withPad: " ", startingAt: 0)) | \($0["numberOfConnections"]!) players" }
+                    .joined(separator: "\n")
+            )
+            ```
+            """
+            fields.append(
+                DiscordEmbedField(
+                    name: "\(server[0]["regionEmoji"]!) \(server[0]["region"]!)",
+                    value: value,
+                    inline: false)
+            )
+        }
+
+        let dateFormatter = ISO8601DateFormatter()
+        return DiscordEmbedResponse(
+            embed: DiscordEmbed(
+                timestamp: "\(dateFormatter.string(from: Date(timeIntervalSince1970: lastUpdate)))",
+                fields: fields
+            )
+        )
     }
 }
 
 struct IndexResponse: Content {
     var servers: [[[String : String]]]
-    var lastUpdate: String
+    var lastUpdate: TimeInterval
 }
 
 struct DiscordEmbedResponse: Content {
@@ -230,7 +185,7 @@ struct DiscordEmbedField: Content {
     var inline: Bool
 }
 
-extension Data {
+private extension Data {
     init?(hex: String) {
         let len = hex.count / 2
         var data = Data(capacity: len)
